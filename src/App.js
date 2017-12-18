db2e43433b9ba807a73e94c5f1569199c2a68a8a
Melody <Melody@Melodys-MacBook.local>
2017-12-18 11:03:10 -0700

    credit card application

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ class App extends Component {
       <div className="App">
         <Header loggedInAs={loggedInAs} isCreditCardHolder={isCreditCardHolder} setLoggedInAs={this.setLoggedInAs}/>
         <MainContent />
-        <Footer />
+        <Footer loggedInAs={loggedInAs} isCreditCardHolder={isCreditCardHolder} setIsCreditCardHolder={this.setIsCreditCardHolder}/>
       </div>
     );
   }
